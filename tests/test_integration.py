abc7c4ff70fbea3e1fb75eceba7f0bebcda0cda6
Rok Povsic <rok.povsic@gmail.com>
2020-04-09 08:53:45 +0200

    Add a multiple requests test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,6 @@
 import os
 import time
+import uuid
 
 from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import service_pb2_grpc, service_pb2, resources_pb2
@@ -23,6 +24,14 @@ def test_post_patch_delete_input_on_json_channel():
 
 def test_post_patch_delete_input_on_grpc_channel():
   _assert_post_patch_delete_input(ClarifaiChannel.get_insecure_grpc_channel())
+
+
+def test_multiple_requests_on_json_channel():
+  _assert_multiple_requests(ClarifaiChannel.get_json_channel())
+
+
+def test_multiple_requests_on_grpc_channel():
+  _assert_multiple_requests(ClarifaiChannel.get_insecure_grpc_channel())
 
 
 def _assert_post_model_outputs(channel):
@@ -94,3 +103,173 @@ def _assert_post_patch_delete_input(channel):
     delete_request = service_pb2.DeleteInputRequest(input_id=input_id)
     delete_response = stub.DeleteInput(delete_request, metadata=metadata)
     assert status_code_pb2.SUCCESS == delete_response.status.code
+
+
+def _assert_multiple_requests(channel):
+  model_id = str(uuid.uuid4())
+
+  metadata = (('authorization', 'Key %s' % os.environ.get('CLARIFAI_API_KEY')),)
+  stub = service_pb2_grpc.V2Stub(channel)
+
+  _raise_on_failure(
+    stub.PostModels(
+      service_pb2.PostModelsRequest(
+        models=[
+          resources_pb2.Model(
+            id=model_id,
+            output_info=resources_pb2.OutputInfo(
+              data=resources_pb2.Data(
+                concepts=[
+                  resources_pb2.Concept(id="dog"),
+                  resources_pb2.Concept(id="toddler"),
+                ]
+              )
+            )
+          )
+        ]
+      ),
+      metadata=metadata)
+  )
+
+  post_inputs_response = stub.PostInputs(
+    service_pb2.PostInputsRequest(
+      inputs=[
+        resources_pb2.Input(
+          data=resources_pb2.Data(
+            image=resources_pb2.Image(
+              url="https://samples.clarifai.com/dog2.jpeg",
+              allow_duplicate_url=True
+            ),
+            concepts=[resources_pb2.Concept(id="dog")],
+          )
+        ),
+        resources_pb2.Input(
+          data=resources_pb2.Data(
+            image=resources_pb2.Image(
+              url="https://samples.clarifai.com/toddler-flowers.jpeg",
+              allow_duplicate_url=True
+            ),
+            concepts=[resources_pb2.Concept(id="toddler")],
+          )
+        ),
+      ]
+    ),
+    metadata=metadata
+  )
+  _raise_on_failure(post_inputs_response)
+
+  input_ids = [i.id for i in post_inputs_response.inputs]
+  _wait_for_inputs_upload(stub, metadata, input_ids)
+
+  response = stub.PostModelVersions(
+    service_pb2.PostModelVersionsRequest(model_id=model_id),
+    metadata=metadata
+  )
+  _raise_on_failure(response)
+
+  model_version_id = response.model.model_version.id
+  _wait_for_model_trained(stub, metadata, model_id, model_version_id)
+
+  _raise_on_failure(stub.PostModelVersionMetrics(
+    service_pb2.PostModelVersionMetricsRequest(
+      model_id=model_id,
+      version_id=model_version_id,
+    ),
+    metadata=metadata
+  ))
+
+  _wait_for_model_evaluated(stub, metadata, model_id, model_version_id)
+
+  response = stub.GetModelVersionMetrics(
+    service_pb2.GetModelVersionMetricsRequest(
+      model_id=model_id,
+      version_id=model_version_id,
+      fields=resources_pb2.FieldsValue(
+        confusion_matrix=True,
+        cooccurrence_matrix=True,
+        label_counts=True,
+        binary_metrics=True,
+        test_set=True,
+      )
+    ),
+    metadata=metadata
+  )
+  _raise_on_failure(response)
+
+  _raise_on_failure(
+    stub.DeleteModel(service_pb2.DeleteModelRequest(model_id=model_id), metadata=metadata)
+  )
+
+  _raise_on_failure(
+    stub.DeleteInputs(service_pb2.DeleteInputsRequest(ids=input_ids), metadata=metadata)
+  )
+
+
+def _wait_for_inputs_upload(stub, metadata, input_ids):
+  for input_id in input_ids:
+    while True:
+      get_input_response = stub.GetInput(
+        service_pb2.GetInputRequest(input_id=input_id),
+        metadata=metadata
+      )
+      _raise_on_failure(get_input_response)
+      if get_input_response.input.status.code == status_code_pb2.INPUT_DOWNLOAD_SUCCESS:
+        break
+      elif get_input_response.input.status.code in (status_code_pb2.INPUT_DOWNLOAD_PENDING,
+                                                    status_code_pb2.INPUT_DOWNLOAD_IN_PROGRESS):
+        time.sleep(1)
+        continue
+      else:
+        raise Exception(
+          get_input_response.status.description + " " + get_input_response.status.details
+        )
+  # At this point, all inputs have been downloaded successfully.
+
+
+def _wait_for_model_trained(stub, metadata, model_id, model_version_id):
+  while True:
+    response = stub.GetModelVersion(
+      service_pb2.GetModelVersionRequest(model_id=model_id, version_id=model_version_id),
+      metadata=metadata
+    )
+    _raise_on_failure(response)
+    if response.model_version.status.code == status_code_pb2.MODEL_TRAINED:
+      break
+    elif response.model_version.status.code in (status_code_pb2.MODEL_QUEUED_FOR_TRAINING,
+                                                status_code_pb2.MODEL_TRAINING):
+      time.sleep(1)
+      continue
+    else:
+      raise Exception(
+        response.status.description + " " + response.status.details
+      )
+  # At this point, the model has successfully finished training.
+
+
+def _wait_for_model_evaluated(stub, metadata, model_id, model_version_id):
+  while True:
+    response = stub.GetModelVersionMetrics(
+      service_pb2.GetModelVersionMetricsRequest(model_id=model_id, version_id=model_version_id),
+      metadata=metadata
+    )
+    _raise_on_failure(response)
+    if response.model_version.metrics.status.code == status_code_pb2.MODEL_EVALUATED:
+      break
+    elif response.model_version.metrics.status.code in (status_code_pb2.MODEL_QUEUED_FOR_EVALUATION,
+                                                        status_code_pb2.MODEL_EVALUATING):
+      time.sleep(1)
+      continue
+    else:
+      raise Exception(
+        response.status.description + " " + response.status.details
+      )
+  # At this point, the model has successfully finished evaluation.
+
+
+def _raise_on_failure(response):
+  if response.status.code != status_code_pb2.SUCCESS:
+    print("Received failure response:")
+    print(response)
+    raise Exception(
+      str(response.status.code) + " " + response.status.description + " " + response.status.details
+    )
