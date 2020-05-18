6cd1ff9a5fe8555d097e7b8534f753b673722e1a
Rok Povsic <rok.povsic@gmail.com>
2020-05-18 18:48:23 +0200

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clarifai-grpc",
-    version="0.0.2",
+    version="0.1.0",
     author="Clarifai",
     author_email="support@clarifai.com",
     description="Clarifai gRPC API Client",
