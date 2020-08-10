d204b63b2664aed634b73db1360e9d0151d3d440
Rok Povsic <rok.povsic@clarifai.com>
2020-08-10 17:27:08 +0200

    Add auth and concepts tests

---
+++ tests/test_auth.py
@@ -0,0 +1,14 @@
+from clarifai_grpc.grpc.api import service_pb2_grpc, service_pb2
+from clarifai_grpc.grpc.api.status import status_code_pb2
+from tests.helpers import both_channels
+
+
+@both_channels
+def test_invalid_api_key(channel):
+  stub = service_pb2_grpc.V2Stub(channel)
+  response = stub.ListModels(
+    service_pb2.ListModelsRequest(),
+    metadata=(('authorization', 'Key SOME_INVALID_KEY'),))
+
+  assert response.status.code == status_code_pb2.StatusCode.CONN_KEY_INVALID
+  assert response.status.description == "Invalid API key or Invalid API key/application pair"
