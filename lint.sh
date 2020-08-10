9a704cb1fc8f3d8ed2736a01e5687b872a39ea37
Rok Povsic <rok.povsic@clarifai.com>
2020-08-10 20:22:35 +0200

    Add autoflake and apply it

--- lint.sh
+++ lint.sh
@@ -4,6 +4,27 @@ set -eo pipefail
 
 FIND_SOURCE_FILES='find clarifai_grpc tests -name "*.py" -not -path "clarifai_grpc/grpc/*"'
 
+function run_autoflake {
+  echo "- autoflake: Make sure there are no unused imports and unused variables"
+  FIND_SOURCE_FILES_FOR_AUTOFLAKE="${FIND_SOURCE_FILES} -not -iwholename '*/__init__.py'"
+  autoflake --remove-all-unused-imports --remove-unused-variables \
+          $(eval ${FIND_SOURCE_FILES_FOR_AUTOFLAKE}) \
+          | tee autoflake-output.tmp
+
+  if [ $(cat autoflake-output.tmp | wc -c) != 0 ]; then
+    echo ""
+    echo "  Here are affected files: "
+    grep "+++" autoflake-output.tmp
+    rm autoflake-output.tmp
+    echo "  autoflake failed"
+    exit 1
+  fi
+
+  rm autoflake-output.tmp
+  echo "  Done autoflake"
+}
+
+
 function run_isort() {
   echo ""
   echo "- isort: Make sure all imports are sorted"
@@ -43,6 +64,7 @@ function run_yapf {
 }
 
 
+run_autoflake
 run_isort $1
 run_yapf $1
 
