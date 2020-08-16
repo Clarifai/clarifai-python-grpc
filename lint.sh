d6abb109710531b1e4d44e602ea981bb51196465
Rok Povsic <rok.povsic@clarifai.com>
2020-08-16 19:34:35 +0200

    Remove using yapf
    The style doesn't work well when specifying gRPC requests

--- lint.sh
+++ lint.sh
@@ -32,40 +32,19 @@ function run_isort() {
   if [ "$1" == "isort" ]; then
     isort --sp .isort.cfg --ws $(eval ${FIND_SOURCE_FILES})
   fi
-  # This ignores whitespace which is crucial because yapf defines the formatting.
+  # This ignores whitespace
   isort --sp .isort.cfg --ws --diff -c $(eval ${FIND_SOURCE_FILES})
 
   if [ $? != 0 ]; then
     echo ""
     echo "  isort failed. Run './lint.sh isort' to automatically sort the imports correctly."
-    echo "    Note: The import code style itself (besides the order) must still comply to yapf"
     exit 1
   fi
 
   echo "  Done isort"
 }
 
-function run_yapf {
-  echo ""
-  echo "- yapf: Make sure there are no code style issues"
-
-  if [ "$1" == "yapf" ]; then
-    yapf --style=.style.yapf -p -i $(eval ${FIND_SOURCE_FILES})
-  fi
-  yapf --style=.style.yapf -p -d $(eval ${FIND_SOURCE_FILES})
-
-  if [ $? != 0 ]; then
-    echo ""
-    echo "  yapf failed. Run './lint.sh yapf' to automatically apply yapf rules."
-    exit 1
-  fi
-
-  echo "  Done aypf"
-}
-
-
 run_autoflake
 run_isort $1
-run_yapf $1
 
 echo "SUCCESSFUL FINISH OF lint.sh"
