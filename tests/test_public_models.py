6b6eabf1275931856930f26b7d2b02fca0206788
Rok Povsic <rok.povsic@clarifai.com>
2020-08-18 22:15:28 +0200

    Add public models predict tests

---
+++ tests/test_public_models.py
@@ -0,0 +1,63 @@
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from tests.common import (
+    DOG_IMAGE_URL,
+    APPAREL_MODEL_ID,
+    CELEBRITY_MODEL_ID,
+    COLOR_MODEL_ID,
+    DEMOGRAPHICS_MODEL_ID,
+    FACE_DETECTION_MODEL_ID,
+    FACE_EMBEDDING_MODEL_ID,
+    FOOD_MODEL_ID,
+    GENERAL_EMBEDDING_MODEL_ID,
+    GENERAL_MODEL_ID,
+    LANDSCAPE_QUALITY_MODEL_ID,
+    LOGO_MODEL_ID,
+    MODERATION_MODEL_ID,
+    NSFW_MODEL_ID,
+    PORTRAIT_QUALITY_MODEL_ID,
+    TEXTURES_AND_PATTERNS_MODEL_ID,
+    TRAVEL_MODEL_ID,
+    WEDDING_MODEL_ID,
+    both_channels,
+    metadata,
+    raise_on_failure,
+)
+
+
+@both_channels
+def test_predict_on_public_models(channel):
+    model_title_and_id_pairs = [
+        ("apparel", APPAREL_MODEL_ID),
+        ("celebrity", CELEBRITY_MODEL_ID),
+        ("color", COLOR_MODEL_ID),
+        ("demographics", DEMOGRAPHICS_MODEL_ID),
+        ("face detection", FACE_DETECTION_MODEL_ID),
+        ("face embedding", FACE_EMBEDDING_MODEL_ID),
+        ("food", FOOD_MODEL_ID),
+        ("general embedding", GENERAL_EMBEDDING_MODEL_ID),
+        ("general", GENERAL_MODEL_ID),
+        ("landscape quality", LANDSCAPE_QUALITY_MODEL_ID),
+        ("logo", LOGO_MODEL_ID),
+        ("moderation", MODERATION_MODEL_ID),
+        ("nsfw", NSFW_MODEL_ID),
+        ("portrait quality", PORTRAIT_QUALITY_MODEL_ID),
+        ("textures and patterns", TEXTURES_AND_PATTERNS_MODEL_ID),
+        ("travel", TRAVEL_MODEL_ID),
+        ("wedding", WEDDING_MODEL_ID),
+    ]
+
+    stub = service_pb2_grpc.V2Stub(channel)
+
+    for title, model_id in model_title_and_id_pairs:
+        request = service_pb2.PostModelOutputsRequest(
+            model_id=model_id,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(image=resources_pb2.Image(url=DOG_IMAGE_URL))
+                )
+            ],
+        )
+        response = stub.PostModelOutputs(request, metadata=metadata())
+        raise_on_failure(
+            response, custom_message=f"Predict failed for the {title} model (ID: {model_id})."
+        )
