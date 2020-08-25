27209ef91c49f228f689e6185daa4730d57fc9c9
Rok Povsic <rok.povsic@clarifai.com>
2020-08-25 16:46:19 +0200

    Add video predict test with custom sample_ms

--- tests/test_video_predict.py
+++ tests/test_video_predict.py
@@ -6,6 +6,7 @@ from tests.common import (
     both_channels,
     metadata,
     raise_on_failure,
+    BEER_VIDEO_URL,
 )
 
 
@@ -79,6 +80,36 @@ def test_predict_video_url_with_max_concepts(channel):
     assert len(response.outputs[0].data.frames) > 0
     for frame in response.outputs[0].data.frames:
         assert len(frame.data.concepts) == 3
+
+
+@both_channels
+def test_predict_video_url_with_custom_sample_ms(channel):
+    stub = service_pb2_grpc.V2Stub(channel)
+
+    request = service_pb2.PostModelOutputsRequest(
+        model_id=GENERAL_MODEL_ID,
+        inputs=[
+            resources_pb2.Input(
+                data=resources_pb2.Data(video=resources_pb2.Video(url=BEER_VIDEO_URL))
+            )
+        ],
+        model=resources_pb2.Model(
+            output_info=resources_pb2.OutputInfo(
+                output_config=resources_pb2.OutputConfig(sample_ms=2000)
+            )
+        ),
+    )
+    response = stub.PostModelOutputs(request, metadata=metadata())
+    raise_on_failure(response)
+
+    # The expected time per frame is the middle between the start and the end of the frame
+    # (in milliseconds).
+    expected_time = 1000
+
+    assert len(response.outputs[0].data.frames) > 0
+    for frame in response.outputs[0].data.frames:
+        assert frame.frame_info.time == expected_time
+        expected_time += 2000
 
 
 @both_channels
