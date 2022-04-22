58dd7ce88f411be854cb45f6891207bd45f09978
Isaac Chung <isaac.chung@clarifai.com>
2022-04-22 19:05:40 +0300

    revert comment

--- tests/test_public_models_predicts.py
+++ tests/test_public_models_predicts.py
@@ -71,47 +71,47 @@ def test_text_predict_on_public_models(channel):
         )
 
 
-# @both_channels
-# def test_image_predict_on_public_models(channel):
-#     stub = service_pb2_grpc.V2Stub(channel)
+@both_channels
+def test_image_predict_on_public_models(channel):
+    stub = service_pb2_grpc.V2Stub(channel)
 
-#     for title, model_id in MODEL_TITLE_AND_ID_PAIRS:
-#         request = service_pb2.PostModelOutputsRequest(
-#             model_id=model_id,
-#             inputs=[
-#                 resources_pb2.Input(
-#                     data=resources_pb2.Data(image=resources_pb2.Image(url=DOG_IMAGE_URL))
-#                 )
-#             ],
-#         )
-#         response = post_model_outputs_and_maybe_allow_retries(stub, request, metadata=metadata())
-#         raise_on_failure(
-#             response,
-#             custom_message=f"Image predict failed for the {title} model (ID: {model_id}).",
-#         )
+    for title, model_id in MODEL_TITLE_AND_ID_PAIRS:
+        request = service_pb2.PostModelOutputsRequest(
+            model_id=model_id,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(image=resources_pb2.Image(url=DOG_IMAGE_URL))
+                )
+            ],
+        )
+        response = post_model_outputs_and_maybe_allow_retries(stub, request, metadata=metadata())
+        raise_on_failure(
+            response,
+            custom_message=f"Image predict failed for the {title} model (ID: {model_id}).",
+        )
 
 
-# @both_channels
-# def test_video_predict_on_public_models(channel):
-#     stub = service_pb2_grpc.V2Stub(channel)
+@both_channels
+def test_video_predict_on_public_models(channel):
+    stub = service_pb2_grpc.V2Stub(channel)
 
-#     models_with_no_video_support = {
-#         "color",
-#     }
+    models_with_no_video_support = {
+        "color",
+    }
 
-#     for title, model_id in MODEL_TITLE_AND_ID_PAIRS:
-#         if title in models_with_no_video_support:
-#             continue
-#         request = service_pb2.PostModelOutputsRequest(
-#             model_id=model_id,
-#             inputs=[
-#                 resources_pb2.Input(
-#                     data=resources_pb2.Data(video=resources_pb2.Video(url=BEER_VIDEO_URL))
-#                 )
-#             ],
-#         )
-#         response = post_model_outputs_and_maybe_allow_retries(stub, request, metadata=metadata())
-#         raise_on_failure(
-#             response,
-#             custom_message=f"Video predict failed for the {title} model (ID: {model_id}).",
-#         )
+    for title, model_id in MODEL_TITLE_AND_ID_PAIRS:
+        if title in models_with_no_video_support:
+            continue
+        request = service_pb2.PostModelOutputsRequest(
+            model_id=model_id,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(video=resources_pb2.Video(url=BEER_VIDEO_URL))
+                )
+            ],
+        )
+        response = post_model_outputs_and_maybe_allow_retries(stub, request, metadata=metadata())
+        raise_on_failure(
+            response,
+            custom_message=f"Video predict failed for the {title} model (ID: {model_id}).",
+        )
