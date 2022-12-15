54758dd2d6a72d86dec1a1c831902e57b686f167
Marco Alonso Perez-Guzman <marco.perez@clarifai.com>
2022-12-14 21:49:13 -0500

    change to separate file

---
+++ tests/client/test_custom_code_operators.py
@@ -0,0 +1,75 @@
+import time
+import uuid
+
+from google.protobuf import struct_pb2
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
+
+from tests.common import (
+    both_channels,
+    post_model_outputs_and_maybe_allow_retries,
+    raise_on_failure,
+    MAX_RETRY_ATTEMPTS,
+    TRUCK_IMAGE_URL,
+    DOG_IMAGE_URL,
+    TEST_OPERATOR_CODE,
+)
+
+@both_channels
+def test_post_predict_delete_custom_code_operator_model(channel):
+    """
+    Add custom code operator model, predict, patch model, predict, delete CCO.
+    """
+    stub = service_pb2_grpc.V2Stub(channel)
+    model_id = "coperator_" + uuid.uuid4().hex[:20]
+
+    output_info_params = struct_pb2.Struct()
+    output_info_params.update({"operator_code": TEST_OPERATOR_CODE})
+    output_info = resources_pb2.OutputInfo(params=output_info_params)
+    model = resources_pb2.Model(
+        id="custom_code_operator_model",
+        model_type_id="custom-code-operator",
+        output_info=output_info,
+    )
+    req = service_pb2.PostModelsRequest(model=model)
+    raise_on_failure(stub.PostModels(req))
+
+    for i in range(0, MAX_RETRY_ATTEMPTS):
+        resp = stub.GetModelVersion(
+            service_pb2.GetModelVersion(
+                model_id=model_id,
+            )
+        )
+        latest_status = resp.model.model_version.status
+        if latest_status != status_code_pb2.MODEL_TRAINED:
+            time.sleep(2)
+
+    assert latest_status == status_code_pb2.MODEL_TRAINED
+
+    inputs = (
+        [
+            resources_pb2.Input(
+                id="321",
+                data=resources_pb2.Data(image=resources_pb2.Image(url=DOG_IMAGE_URL)),
+            ),
+            resources_pb2.Input(
+                id="123",
+                data=resources_pb2.Data(image=resources_pb2.Image(url=TRUCK_IMAGE_URL)),
+            ),
+        ],
+    )
+
+    req = service_pb2.PostModelOutputsRequest(model_id=model_id, inputs=inputs)
+    response = post_model_outputs_and_maybe_allow_retries(stub=stub, request=req, metadata=())
+
+    raise_on_failure(response)
+    assert len(response["outputs"]) == 2
+    assert len(response["outputs"][0]["data"]["metadata"]) == 1
+    assert len(response["outputs"][1]["data"]["metadata"]) == 1
+    assert (
+        response["outputs"][0]["data"]["metadata"]["processed_at"]
+        < response["outputs"][1]["data"]["metadata"]["processed_at"]
+    )
+
+    raise_on_failure(stub.DeleteModel(service_pb2.DeleteModelRequest(model_id=model_id)))
+
--- tests/client/test_model_crud.py
+++ tests/client/test_model_crud.py
@@ -1,23 +1,18 @@
-import time
 import uuid
 
 import pytest
 from google.protobuf import struct_pb2
 
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
-from clarifai_grpc.grpc.api.status import status_code_pb2
 from tests.common import (
     both_channels,
     metadata,
-    post_model_outputs_and_maybe_allow_retries,
     raise_on_failure,
     wait_for_model_evaluated,
     wait_for_model_trained,
     wait_for_inputs_upload,
-    MAX_RETRY_ATTEMPTS,
     TRUCK_IMAGE_URL,
     DOG_IMAGE_URL,
-    TEST_OPERATOR_CODE,
 )
 
 
@@ -162,69 +157,9 @@ def test_post_patch_get_train_evaluate_predict_delete_model(channel):
         raise_on_failure(delete_response)
 
         delete_inputs_response = stub.DeleteInputs(
-            service_pb2.DeleteInputsRequest(ids=[input_id_1, input_id_2]),
-            metadata=metadata(),
+            service_pb2.DeleteInputsRequest(ids=[input_id_1, input_id_2]), metadata=metadata()
         )
         raise_on_failure(delete_inputs_response)
-
-
-@both_channels
-def test_post_predict_delete_custom_code_operator_model(channel):
-    """
-    Add custom code operator model, predict, patch model, predict, delete CCO.
-    """
-    stub = service_pb2_grpc.V2Stub(channel)
-    model_id = "coperator_" + uuid.uuid4().hex[:20]
-
-    output_info_params = struct_pb2.Struct()
-    output_info_params.update({"operator_code": TEST_OPERATOR_CODE})
-    output_info = resources_pb2.OutputInfo(params=output_info_params)
-    model = resources_pb2.Model(
-        id="custom_code_operator_model",
-        model_type_id="custom-code-operator",
-        output_info=output_info,
-    )
-    req = service_pb2.PostModelsRequest(model=model)
-    raise_on_failure(stub.PostModels(req))
-
-    for i in range(0, MAX_RETRY_ATTEMPTS):
-        resp = stub.GetModelVersion(
-            service_pb2.GetModelVersion(
-                model_id=model_id,
-            )
-        )
-        latest_status = resp.model.model_version.status
-        if latest_status != status_code_pb2.MODEL_TRAINED:
-            time.sleep(2)
-
-    assert latest_status == status_code_pb2.MODEL_TRAINED
-
-    inputs = (
-        [
-            resources_pb2.Input(
-                id="321",
-                data=resources_pb2.Data(image=resources_pb2.Image(url=DOG_IMAGE_URL)),
-            ),
-            resources_pb2.Input(
-                id="123",
-                data=resources_pb2.Data(image=resources_pb2.Image(url=TRUCK_IMAGE_URL)),
-            ),
-        ],
-    )
-
-    req = service_pb2.PostModelOutputsRequest(model_id=model_id, inputs=inputs)
-    response = post_model_outputs_and_maybe_allow_retries(stub=stub, request=req, metadata=())
-
-    raise_on_failure(response)
-    assert len(response["outputs"]) == 2
-    assert len(response["outputs"][0]["data"]["metadata"]) == 1
-    assert len(response["outputs"][1]["data"]["metadata"]) == 1
-    assert (
-        response["outputs"][0]["data"]["metadata"]["processed_at"]
-        < response["outputs"][1]["data"]["metadata"]["processed_at"]
-    )
-
-    raise_on_failure(stub.DeleteModel(service_pb2.DeleteModelRequest(model_id=model_id)))
 
 
 @both_channels
