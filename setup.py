6c9a819080c9ed8670d261d15907694cf19192e5
Matthew Zeiler <zeiler@clarifai.com>
2022-12-23 15:26:19 -0500

    updated requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+  long_description = fh.read()
 
 packages = setuptools.find_packages(include=["clarifai_grpc*"])
 
@@ -24,11 +24,11 @@ setuptools.setup(
     license="Apache 2.0",
     python_requires='>=3.6',
     install_requires=[
-        "grpcio>=1.44.0",
-        "protobuf>= 3.15.6, <=3.19.3",
-        "googleapis-common-protos>=1.53.0",
-        "requests>=2.25.1",
+        "grpcio>=1.51.0",
+        "protobuf>= 4.21.12",
+        "googleapis-common-protos>=1.57.0",
+        "requests>=2.28.1",
     ],
-    package_data={p: ["*.pyi"] for p in packages},
-    include_package_data=True
-)
+    package_data={p: ["*.pyi"]
+                  for p in packages},
+    include_package_data=True)
