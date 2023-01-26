17f2a2fba3290e38ba9d583f4c05995dba8764d9
Marco Alonso Perez-Guzman <marco.perez@clarifai.com>
2023-01-26 16:17:30 -0500

    lint

--- tests/client/test_secure_data_hosting.py
+++ tests/client/test_secure_data_hosting.py
@@ -2,38 +2,47 @@ import requests
 import hashlib
 
 from clarifai_grpc.grpc.api import service_pb2_grpc, service_pb2, resources_pb2
-from tests.common import  (
-	TRUCK_IMAGE_URL,
-	TRAVEL_IMAGE_URL,
-	BEER_VIDEO_URL,
-	both_channels,
-	#metadata,
-	secure_data_hosting_metadata, # make this a dict with all the headers we want to test
-	raise_on_failure,
-	wait_for_inputs_upload,
+from tests.common import (
+    TRUCK_IMAGE_URL,
+    TRAVEL_IMAGE_URL,
+    BEER_VIDEO_URL,
+    both_channels,
+    # metadata,
+    # secure_data_hosting_metadata, # make this a dict with all the headers we want to test
+    raise_on_failure,
+    wait_for_inputs_upload,
 )
 
 ########
-#TODO: make sure we use different authentication methods
+# TODO: make sure we use different authentication methods
 def metadata(pat=False):
     if pat:
-        return (("authorization", "Key %s" % os.environ.get("CLARIFAI_PAT_KEY_SECURE_DATA_HOSTING")),)
+        return (
+            ("authorization", "Key %s" % os.environ.get("CLARIFAI_PAT_KEY_SECURE_DATA_HOSTING")),
+        )
     else:
-        return (("authorization", "Key %s" % os.environ.get("CLARIFAI_API_KEY_SECURE_DATA_HOSTING")),)
+        return (
+            ("authorization", "Key %s" % os.environ.get("CLARIFAI_API_KEY_SECURE_DATA_HOSTING")),
+        )
+
 
 def get_bytes_hash(url):
     r = requests.get(url, stream=True)
     return hashlib.md5(r.raw.data).hexdigest()
 
-env_subdomain = os.environ.get("CLARIFAI_GRPC_BASE", "api.clarifai.com").split('.')[0]
+
+env_subdomain = os.environ.get("CLARIFAI_GRPC_BASE", "api.clarifai.com").split(".")[0]
 if env_subdomain == "api-dev":
     default_secure_data_hosting_url = "https//data-dev.clarifai.com"
 if env_subdomain == "api-staging":
     default_secure_data_hosting_url = "https//data-staging.clarifai.com"
 else:
     default_secure_data_hosting_url = "https//data.clarifai.com"
-secure_data_hosting_url = os.envrion.get("CLARIFAI_SECURE_DATA_HOSTING_URL", default_secure_data_hosting_url)
+secure_data_hosting_url = os.envrion.get(
+    "CLARIFAI_SECURE_DATA_HOSTING_URL", default_secure_data_hosting_url
+)
 ########
+
 
 @both_channels
 def test_adding_inputs(channel):
@@ -54,47 +63,44 @@ def test_adding_inputs(channel):
 
     # Get app req to fetch custom-facing user/app ids
     get_user_response = stub.GetUser(
-        service_pb2.GetUserRequest(
-           user_app_id=resources_pn2.UserAppIDSet(user_id="me") 
-	),
-	metadata=metadata(pat=True),
-
+        service_pb2.GetUserRequest(user_app_id=resources_pn2.UserAppIDSet(user_id="me")),
+        metadata=metadata(pat=True),
     )
     raise_on_failure(get_user_response)
 
     # needed to build secure hosting url
-    user_cfid = get_user_response.user.id # get user cfid
+    user_cfid = get_user_response.user.id  # get user cfid
     app_cfid = os.environ["CLARIFAI_APP_ID_SECURE_HOSTING"]
 
     post_image_response = stub.PostInputs(
         service_pb2.PostInputsRequest(
             inputs=[
                 resources_pb2.Input(
-		    id=input_img1,
+                    id=input_img1,
                     data=resources_pb2.Data(
                         image=resources_pb2.Image(url=TRUCK_IMAGE_URL, allow_duplicate_url=True),
-                    )
-                ),   
+                    ),
+                ),
                 resources_pb2.Input(
-		    id=input_img2,
+                    id=input_img2,
                     data=resources_pb2.Data(
                         image=resources_pb2.Image(url=TRAVEL_IMAGE_URL, allow_duplicate_url=True),
-                    )   
-                )
-            ]   
-        ),  
+                    ),
+                ),
+            ]
+        ),
         metadata=metadata(),
-    )   
+    )
     raise_on_failure(post_image_response)
 
     post_video_response = stub.PostInputs(
         service_pb2.PostInputsRequest(
             inputs=[
                 resources_pb2.Input(
-		    id=input_vid1,
+                    id=input_vid1,
                     data=resources_pb2.Data(
-                        video=image=resources_pb2.Video(url=BEER_VIDEO_URL, allow_duplicate_url=True), 
-                    )
+                        video=resources_pb2.Video(url=BEER_VIDEO_URL, allow_duplicate_url=True),
+                    ),
                 )
             ]
         ),
@@ -105,48 +111,61 @@ def test_adding_inputs(channel):
     try:
         wait_for_inputs_upload(stub, metadata(), inputs)
 
-	for inp in inputs:
+        for inp in inputs:
             get_input_response = stub.GetInput(
                 service_pb2.GetInputRequest(input_id=inp), metadata=metadata()
-            )   
+            )
             raise_on_failure(get_input_response)
 
-            input_type = 'image' if 'img' in inp else 'video'
+            input_type = "image" if "img" in inp else "video"
             if "img" in inp:
-                sizes = ['orig', 'small', 'large', 'tiny']
+                sizes = ["orig", "small", "large", "tiny"]
             elif "vid" in inp:
-                sizes = ['orig', 'thumbnail']
+                sizes = ["orig", "thumbnail"]
             for size in sizes:
-                expected_input_url = get_expected_input_url(inp, app_cfid, user_cfid, size, input_type, bytes_data_hashes[inp])
-                input_url = os.path.join(get_input_response.input.data.image.hosted.prefix, size, get_input_response.input.data.image.hosted.suffix)
-		assert expected_input_url == input_url, 'URLs didnt match'
+                expected_input_url = get_expected_input_url(
+                    inp, app_cfid, user_cfid, size, input_type, bytes_data_hashes[inp]
+                )
+                input_url = os.path.join(
+                    get_input_response.input.data.image.hosted.prefix,
+                    size,
+                    get_input_response.input.data.image.hosted.suffix,
+                )
+        assert expected_input_url == input_url, "URLs didnt match"
 
         list_inputs_response = stub.ListInputs(
             service_pb2.ListInputsRequest(per_page=1), metadata=metadata()
-        )   
+        )
         raise_on_failure(list_inputs_response)
         assert len(list_inputs_response.inputs) == len(inputs)
     finally:
-	for inp in inputs:
+        for inp in inputs:
             delete_request = service_pb2.DeleteInputRequest(input_id=inp)
             delete_response = stub.DeleteInput(delete_request, metadata=metadata())
             raise_on_failure(delete_response)
 
+
 def get_expected_input_urls(input_id, app_cfid, user_cfid, size, input_type, filename):
-    expected_input_url = "%s/%s/users/%s/apps/%s/inputs/%s/%s" % (secure_data_hosting_url, size, user_cfid, app_cfid, input_type, filename)
+    expected_input_url = "%s/%s/users/%s/apps/%s/inputs/%s/%s" % (
+        secure_data_hosting_url,
+        size,
+        user_cfid,
+        app_cfid,
+        input_type,
+        filename,
+    )
     return expected_input_url
 
-	# TODO: Get password of current test user (and reuse it?) -- done
-	# TODO: Create User in dev/staging/prod (delete suffixed emails) -- done
-	# TODO: Use admin API to enable secure data hosting -- done
-	# TODO: Add github secret for CLARIFAI_USER_EMAIL_SECURE_HOSTING to add to ~/work/clarifai-python-grpc/.github/workflows/run_tests.yml -- done
-	# TODO: Update secure data hosting version of CLARIFAI_APP_ID, CLARIFAI_API_KEY, CLARIFAI_PAT_KEY in -- done
-	# write tests:
-		# POST inputs to the app
-		# list inputs for the app
-		# get individual inputs
-		# fetch inputs with requests library
+    # TODO: Get password of current test user (and reuse it?) -- done
+    # TODO: Create User in dev/staging/prod (delete suffixed emails) -- done
+    # TODO: Use admin API to enable secure data hosting -- done
+    # TODO: Add github secret for CLARIFAI_USER_EMAIL_SECURE_HOSTING to add to ~/work/clarifai-python-grpc/.github/workflows/run_tests.yml -- done
+    # TODO: Update secure data hosting version of CLARIFAI_APP_ID, CLARIFAI_API_KEY, CLARIFAI_PAT_KEY in -- done
+    # write tests:
+    # POST inputs to the app
+    # list inputs for the app
+    # get individual inputs
+    # fetch inputs with requests library
 
-
-	# TODO: Add new secret for CLARIFAI_USER_EMAIL_SECURE_HOSTING in ~/work/crons-api-client-tests/.github/workflows/grpc-python-client-test.yml 
-	# 		both run_tests.yml and grpc-python-client-test.yml 
+    # TODO: Add new secret for CLARIFAI_USER_EMAIL_SECURE_HOSTING in ~/work/crons-api-client-tests/.github/workflows/grpc-python-client-test.yml
+    #         both run_tests.yml and grpc-python-client-test.yml
