eb83792cd4e16311d93eaac0dc7b9c1cb7794b57
Marco Alonso Perez-Guzman <marco.perez@clarifai.com>
2023-01-27 02:09:39 -0500

    make consts for header types

--- tests/client/test_secure_data_hosting.py
+++ tests/client/test_secure_data_hosting.py
@@ -18,9 +18,20 @@ PAT = os.environ.get("CLARIFAI_PAT_KEY_SECURE_HOSTING")
 API_KEY = os.environ.get("CLARIFAI_API_KEY_SECURE_HOSTING")
 SESSION_TOKEN = os.environ.get("CLARIFAI_SESSION_TOKEN_SECURE_HOSTING")
 
-PAT_AUTH_HEADER = (("authorization", "Key %s" % PAT),)
-API_AUTH_HEADER = (("authorization", "Key %s" % API_KEY),)
+PAT_CLIENT_AUTH = (("authorization", "Key %s" % PAT),)
+API_CLIENT_AUTH = (("authorization", "Key %s" % API_KEY),)
 
+HTTP_AUTH_HEADERS = {
+    'session_token_header': {"X-Clarifai-Session-Token": SESSION_TOKEN}
+    'api_key_header': {API_CLIENT_AUTH[0][0]: API_CLIENT_AUTH[0][1]}
+    'pat_header': {PAT_CLIENT_AUTH[0][0]: PAT_CLIENT_AUTH[0][1]}
+}
+
+HTTP_COOKIE_HEADERS = {
+    'session_token_cookie': {"x_clarifai_session_token": SESSION_TOKEN}
+    'api_key_cookie': {"x-clarifai-api-key": API_KEY}
+    'pat_cookie': {"x-clarifai-api-key": PAT}
+}
 
 def get_secure_hosting_url():
     default_secure_data_hosting_url = "https://data.clarifai.com"
@@ -31,11 +42,9 @@ def get_secure_hosting_url():
         default_secure_data_hosting_url = "https://data-staging.clarifai.com"
     return os.environ.get("CLARIFAI_SECURE_HOSTING_URL", default_secure_data_hosting_url)
 
-
 def get_bytes_hash_from_url(url):
     r = req_session.get(url, stream=True)
     return hashlib.md5(r.raw.data).hexdigest()
-
 
 def get_rehost_sizes(input_type):
     if input_type == "image":
@@ -43,7 +52,6 @@ def get_rehost_sizes(input_type):
     elif input_type == "video":
         sizes = ["orig", "thumbnail"]
     return sizes
-
 
 def build_rehost_url_from_api_input(api_input, size, input_type):
     if input_type == "image":
@@ -59,23 +67,13 @@ def build_rehost_url_from_api_input(api_input, size, input_type):
             api_input.data.video.hosted.suffix,
         )
 
-
 def verify_url_with_all_auths(expected_input_url):
-    session_token_cookie = {"x_clarifai_session_token": SESSION_TOKEN}
-    session_token_header = {"X-Clarifai-Session-Token": SESSION_TOKEN}
-    api_key_cookie = {"x-clarifai-api-key": API_KEY}
-    api_key_header = {API_AUTH_HEADER[0][0]: API_AUTH_HEADER[0][1]}
-    pat_cookie = {"x-clarifai-api-key": PAT}
-    pat_header = {PAT_AUTH_HEADER[0][0]: PAT_AUTH_HEADER[0][1]}
-    headers = [session_token_header, api_key_header, pat_header]
-    cookies = [session_token_cookie, api_key_cookie, pat_cookie]
-    for header in headers:
+    for header_type, header in HTTP_AUTH_HEADERS.items():
         r = req_session.get(expected_input_url, stream=True, headers=header)
-        assert len(r.raw.data) != 0, f"No data was fetched from URL {expected_input_url}"
-    for cookie in cookies:
+        assert len(r.raw.data) != 0, f"No data was fetched from URL {expected_input_url}; header type: {header_type}"
+    for cookie_type, cookie in HTTP_COOKIE_HEADERS.items():
         r = req_session.get(expected_input_url, stream=True, cookies=cookie)
-        assert len(r.raw.data) != 0, f"No data was fetched from URL {expected_input_url}"
-
+        assert len(r.raw.data) != 0, f"No data was fetched from URL {expected_input_url}; cookie type: {cookie_type}"
 
 @both_channels
 def test_adding_inputs(channel):
@@ -102,7 +100,7 @@ def test_adding_inputs(channel):
         service_pb2.GetAppRequest(
             user_app_id=resources_pb2.UserAppIDSet(user_id="me", app_id=app_cfid)
         ),
-        metadata=PAT_AUTH_HEADER,
+        metadata=PAT_CLIENT_AUTH,
     )
     raise_on_failure(get_app_response)
     user_cfid = get_app_response.app.user_id  # needed to build expected secure url
@@ -124,7 +122,7 @@ def test_adding_inputs(channel):
                 ),
             ]
         ),
-        metadata=API_AUTH_HEADER,
+        metadata=API_CLIENT_AUTH,
     )
     raise_on_failure(post_image_response)
 
@@ -139,15 +137,15 @@ def test_adding_inputs(channel):
                 )
             ]
         ),
-        metadata=API_AUTH_HEADER,
+        metadata=API_CLIENT_AUTH,
     )
     raise_on_failure(post_video_response)
 
-    wait_for_inputs_upload(stub, API_AUTH_HEADER, bytes_data_hash_by_id.keys())
+    wait_for_inputs_upload(stub, API_CLIENT_AUTH, bytes_data_hash_by_id.keys())
 
     # list input
     list_inputs_response = stub.ListInputs(
-        service_pb2.ListInputsRequest(per_page=10), metadata=API_AUTH_HEADER
+        service_pb2.ListInputsRequest(per_page=10), metadata=API_CLIENT_AUTH
     )
     raise_on_failure(list_inputs_response)
     assert len(list_inputs_response.inputs) == len(bytes_data_hash_by_id.keys())
@@ -155,7 +153,7 @@ def test_adding_inputs(channel):
     for api_input in list_inputs_response.inputs:
         input_id = api_input.id
         get_input_response = stub.GetInput(
-            service_pb2.GetInputRequest(input_id=input_id), metadata=API_AUTH_HEADER
+            service_pb2.GetInputRequest(input_id=input_id), metadata=API_CLIENT_AUTH
         )
         raise_on_failure(get_input_response)
 
@@ -176,7 +174,7 @@ def test_adding_inputs(channel):
     # delete inputs
     for inp in bytes_data_hash_by_id.keys():
         delete_request = service_pb2.DeleteInputRequest(input_id=inp)
-        delete_response = stub.DeleteInput(delete_request, metadata=API_AUTH_HEADER)
+        delete_response = stub.DeleteInput(delete_request, metadata=API_CLIENT_AUTH)
         raise_on_failure(delete_response)
 
 
