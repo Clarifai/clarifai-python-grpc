f9b997027d570ad9f63636cd63a37f5a76a86440
Marco Alonso Perez-Guzman <marco.perez@clarifai.com>
2023-01-29 23:49:12 -0500

    add deletion of inputs in finally block

--- tests/client/test_secure_data_hosting.py
+++ tests/client/test_secure_data_hosting.py
@@ -123,74 +123,88 @@ def test_adding_inputs(channel):
     raise_on_failure(get_app_response)
     user_cfid = get_app_response.app.user_id  # needed to build expected secure url
 
-    post_image_response = stub.PostInputs(
-        service_pb2.PostInputsRequest(
-            inputs=[
-                resources_pb2.Input(
-                    id=input_img1,
-                    data=resources_pb2.Data(
-                        image=resources_pb2.Image(url=TRUCK_IMAGE_URL, allow_duplicate_url=True),
+    try:
+        post_image_response = stub.PostInputs(
+            service_pb2.PostInputsRequest(
+                inputs=[
+                    resources_pb2.Input(
+                        id=input_img1,
+                        data=resources_pb2.Data(
+                            image=resources_pb2.Image(
+                                url=TRUCK_IMAGE_URL, allow_duplicate_url=True
+                            ),
+                        ),
                     ),
-                ),
-                resources_pb2.Input(
-                    id=input_img2,
-                    data=resources_pb2.Data(
-                        image=resources_pb2.Image(url=TRAVEL_IMAGE_URL, allow_duplicate_url=True),
+                    resources_pb2.Input(
+                        id=input_img2,
+                        data=resources_pb2.Data(
+                            image=resources_pb2.Image(
+                                url=TRAVEL_IMAGE_URL, allow_duplicate_url=True
+                            ),
+                        ),
                     ),
-                ),
-            ]
-        ),
-        metadata=API_CLIENT_AUTH,
-    )
-    raise_on_failure(post_image_response)
-
-    post_video_response = stub.PostInputs(  # videos must be uploaded individually
-        service_pb2.PostInputsRequest(
-            inputs=[
-                resources_pb2.Input(
-                    id=input_vid1,
-                    data=resources_pb2.Data(
-                        video=resources_pb2.Video(url=BEER_VIDEO_URL, allow_duplicate_url=True),
-                    ),
-                )
-            ]
-        ),
-        metadata=API_CLIENT_AUTH,
-    )
-    raise_on_failure(post_video_response)
-
-    wait_for_inputs_upload(stub, API_CLIENT_AUTH, bytes_data_hash_by_id.keys())
-
-    # list input
-    list_inputs_response = stub.ListInputs(
-        service_pb2.ListInputsRequest(per_page=10), metadata=API_CLIENT_AUTH
-    )
-    raise_on_failure(list_inputs_response)
-    assert len(list_inputs_response.inputs) == len(bytes_data_hash_by_id.keys())
-    # verify get and list inputs are fetchable and have the correct data
-    for api_input in list_inputs_response.inputs:
-        input_id = api_input.id
-        get_input_response = stub.GetInput(
-            service_pb2.GetInputRequest(input_id=input_id), metadata=API_CLIENT_AUTH
+                ]
+            ),
+            metadata=API_CLIENT_AUTH,
         )
-        raise_on_failure(get_input_response)
+        raise_on_failure(post_image_response)
 
-        input_type = media_type_by_id[input_id]
-        sizes = get_rehost_sizes(input_type)
-        for size in sizes:
-            expected_input_url = get_expected_input_url(
-                input_id, app_cfid, user_cfid, size, input_type, bytes_data_hash_by_id[input_id]
-            )
-            input_url_from_list = build_rehost_url_from_api_input(api_input, size, input_type)
-            assert expected_input_url == input_url_from_list, "URL from List Inputs didnt match"
-            input_url_from_get = build_rehost_url_from_api_input(
-                get_input_response.input, size, input_type
-            )
-            assert expected_input_url == input_url_from_get, "URL fron Get Input didnt match"
-            verify_url_with_all_auths(expected_input_url)
+        post_video_response = stub.PostInputs(  # videos must be uploaded individually
+            service_pb2.PostInputsRequest(
+                inputs=[
+                    resources_pb2.Input(
+                        id=input_vid1,
+                        data=resources_pb2.Data(
+                            video=resources_pb2.Video(
+                                url=BEER_VIDEO_URL, allow_duplicate_url=True
+                            ),
+                        ),
+                    )
+                ]
+            ),
+            metadata=API_CLIENT_AUTH,
+        )
+        raise_on_failure(post_video_response)
 
-    # delete inputs
-    for inp in bytes_data_hash_by_id.keys():
-        delete_request = service_pb2.DeleteInputRequest(input_id=inp)
-        delete_response = stub.DeleteInput(delete_request, metadata=API_CLIENT_AUTH)
-        raise_on_failure(delete_response)
+        wait_for_inputs_upload(stub, API_CLIENT_AUTH, bytes_data_hash_by_id.keys())
+
+        # list input
+        list_inputs_response = stub.ListInputs(
+            service_pb2.ListInputsRequest(per_page=10), metadata=API_CLIENT_AUTH
+        )
+        raise_on_failure(list_inputs_response)
+        assert len(list_inputs_response.inputs) == len(bytes_data_hash_by_id.keys())
+        # verify get and list inputs are fetchable and have the correct data
+        for api_input in list_inputs_response.inputs:
+            input_id = api_input.id
+            get_input_response = stub.GetInput(
+                service_pb2.GetInputRequest(input_id=input_id), metadata=API_CLIENT_AUTH
+            )
+            raise_on_failure(get_input_response)
+
+            input_type = media_type_by_id[input_id]
+            sizes = get_rehost_sizes(input_type)
+            for size in sizes:
+                expected_input_url = get_expected_input_url(
+                    input_id,
+                    app_cfid,
+                    user_cfid,
+                    size,
+                    input_type,
+                    bytes_data_hash_by_id[input_id],
+                )
+                input_url_from_list = build_rehost_url_from_api_input(api_input, size, input_type)
+                assert (
+                    expected_input_url == input_url_from_list
+                ), "URL from List Inputs didnt match"
+                input_url_from_get = build_rehost_url_from_api_input(
+                    get_input_response.input, size, input_type
+                )
+                assert expected_input_url == input_url_from_get, "URL fron Get Input didnt match"
+                verify_url_with_all_auths(expected_input_url)
+    finally:
+        # delete inputs
+        for inp in bytes_data_hash_by_id.keys():
+            delete_request = service_pb2.DeleteInputRequest(input_id=inp)
+            delete_response = stub.DeleteInput(delete_request, metadata=API_CLIENT_AUTH)
+            raise_on_failure(delete_response)
