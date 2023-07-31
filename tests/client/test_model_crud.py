76cbde646cc2fb6d6787897232b962e198668efb
Harmit Minhas <harmit@clarifai.com>
2023-07-31 10:55:13 -0400

    update

--- tests/client/test_model_crud.py
+++ tests/client/test_model_crud.py
@@ -143,7 +143,9 @@ def test_post_patch_get_train_evaluate_predict_delete_model(channel):
         assert get_response.model.id == model_id
         assert get_response.model.name == "some new name"
         assert len(get_response.model.model_version.output_info.data.concepts) == 1
-        assert get_response.model.model_version.output_info.data.concepts[0].id == "some-new-concept"
+        assert (
+            get_response.model.model_version.output_info.data.concepts[0].id == "some-new-concept"
+        )
 
         post_model_version_metrics_response = stub.PostModelVersionMetrics(
             service_pb2.PostModelVersionMetricsRequest(
