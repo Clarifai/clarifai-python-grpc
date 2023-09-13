696d5797b95b4f089bc0826cb4ffcc12541c46af
clarifai-prod <clarifai-prod@users.noreply.github.com>
2023-09-13 11:59:24 -0400

    GRPC clients version 9.8.2

--- clarifai_grpc/channel/http_client.py
+++ clarifai_grpc/channel/http_client.py
@@ -8,7 +8,7 @@ import requests
 
 from clarifai_grpc.channel.errors import ApiError
 
-CLIENT_VERSION = "9.8.1"
+CLIENT_VERSION = "9.8.2"
 OS_VER = os.sys.platform
 PYTHON_VERSION = ".".join(
     map(
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(include=["clarifai_grpc*"])
 
 setuptools.setup(
     name="clarifai-grpc",
-    version="9.8.1",
+    version="9.8.2",
     author="Clarifai",
     author_email="support@clarifai.com",
     description="Clarifai gRPC API Client",
