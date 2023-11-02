6bfd89377ccb778c78ad981ac4398243cf554bf9
clarifai-prod <clarifai-prod@users.noreply.github.com>
2023-11-02 15:17:54 +0000

    GRPC clients version 9.10.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(include=["clarifai_grpc*"])
 
 setuptools.setup(
     name="clarifai-grpc",
-    version="9.9.5",
+    version="9.10.0",
     author="Clarifai",
     author_email="support@clarifai.com",
     description="Clarifai gRPC API Client",
