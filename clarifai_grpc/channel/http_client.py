37655d3b70ae4726306eb6b7cf14dd9c2519b3b8
Marcel <Marcel.Moldovan@clarifai.com>
2024-04-25 21:21:46 +0300

    Increase version (#177)

--- clarifai_grpc/channel/http_client.py
+++ clarifai_grpc/channel/http_client.py
@@ -8,7 +8,7 @@ import requests
 
 from clarifai_grpc.channel.errors import ApiError
 
-CLIENT_VERSION = "10.3.3"
+CLIENT_VERSION = "10.3.4"
 OS_VER = os.sys.platform
 PYTHON_VERSION = ".".join(
     map(
