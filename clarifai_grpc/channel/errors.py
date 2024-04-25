324621dabed445aa4b8d4dd172004bd07957b4e6
Marcel Moldovan <marcel.moldovan@clarifai.com>
2024-04-25 19:24:46 +0200

    Fix formatting

--- clarifai_grpc/channel/errors.py
+++ clarifai_grpc/channel/errors.py
@@ -1,10 +1,10 @@
 class ApiError(Exception):
     pass
 
-class NotImplementedCaller:
 
-  def __call__(self, *args, **kwargs):
-    raise NotImplementedError()
+class NotImplementedCaller:
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError()
 
 
 class UsageError(Exception):
