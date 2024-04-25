19f9c4c6b1212a6f688485131d9b98936d0eec71
Marcel Moldovan <marcel.moldovan@clarifai.com>
2024-04-25 19:19:46 +0200

    [TP-1454] Fix channel missing attributes

--- clarifai_grpc/channel/errors.py
+++ clarifai_grpc/channel/errors.py
@@ -1,6 +1,11 @@
 class ApiError(Exception):
     pass
 
+class NotImplementedCaller:
+
+  def __call__(self, *args, **kwargs):
+    raise NotImplementedError()
+
 
 class UsageError(Exception):
     pass
