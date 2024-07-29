f292167508da2db6772dbd69e3dc5957acaaeb90
Isaac Chung <chungisaac1217@gmail.com>
2024-07-29 11:59:58 +0300

    [EAGLE-4649] add retry to wait_for_model_trained (#190)
    * add retry
    * lint

--- tests/common.py
+++ tests/common.py
@@ -122,7 +122,10 @@ def wait_for_inputs_delete(stub, input_ids, metadata):
         raise Exception(f"Timeout after {timeout} seconds to delete inputs {remaining_input_ids}")
 
 
-def wait_for_model_trained(stub, metadata, model_id, model_version_id, user_app_id=None):
+def wait_for_model_trained(
+    stub, metadata, model_id, model_version_id, user_app_id=None, retry_on_internal_failure=False
+):
+    retry_count = 0
     while True:
         response = stub.GetModelVersion(
             service_pb2.GetModelVersionRequest(
@@ -130,6 +133,16 @@ def wait_for_model_trained(stub, metadata, model_id, model_version_id, user_app_
             ),
             metadata=metadata,
         )
+        ## Temp fix for EAGLE-4649 until LT-5281 is implemented.
+        if (
+            retry_on_internal_failure
+            and response.status.code == status_code_pb2.INTERNAL_UNCATEGORIZED
+            and retry_count < 3
+        ):
+            print(f"Retrying: {response.status}")
+            retry_count += 1
+            continue
+
         raise_on_failure(response)
         if response.model_version.status.code == status_code_pb2.MODEL_TRAINED:
             break
