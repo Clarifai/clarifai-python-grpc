ef1c243b8d60babc9a2dbdadb03884971930bf34
clarifai-prod <clarifai-prod@users.noreply.github.com>
2024-09-07 00:34:39 +0000

    GRPC clients version 10.8.4

--- clarifai_grpc/channel/http_client.py
+++ clarifai_grpc/channel/http_client.py
@@ -8,7 +8,7 @@ import requests
 
 from clarifai_grpc.channel.errors import ApiError
 
-CLIENT_VERSION = "10.8.3"
+CLIENT_VERSION = "10.8.4"
 OS_VER = os.sys.platform
 PYTHON_VERSION = ".".join(
     map(
