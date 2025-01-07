fe33a1457b0dfd0510fae8e0ea13e16a4e66af27
Matthew Zeiler <zeiler@clarifai.com>
2025-01-07 10:02:37 -0500

    [EAGLE-5310]: 10.11.3 Update clarifai_channel.py retry latency (#200)
    * Update clarifai_channel.py retry latency
    
    * bump version as well

--- clarifai_grpc/channel/http_client.py
+++ clarifai_grpc/channel/http_client.py
@@ -8,7 +8,7 @@ import requests
 
 from clarifai_grpc.channel.errors import ApiError
 
-CLIENT_VERSION = "10.11.2"
+CLIENT_VERSION = "10.11.3"
 OS_VER = os.sys.platform
 PYTHON_VERSION = ".".join(
     map(
