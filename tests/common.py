d3a267417241a3076fa62ce4148673ab7fe9bc11
Marcel Moldovan <marcel.moldovan@clarifai.com>
2025-07-12 21:29:51 +0300

    Fix import order

--- tests/common.py
+++ tests/common.py
@@ -1,9 +1,9 @@
 import os
-import pytest
 import time
 from datetime import datetime
 from typing import List, Tuple
 
+import pytest
 from grpc._channel import _Rendezvous
 
 from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
